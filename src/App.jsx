95e7555400978eb7c237c3077e26561d4b97026f
PrAndrian <princydruh@gmail.com>
2023-08-08 21:43:43 +0200

    Routes added + components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import './App.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Error404 from "./pages/Error404";
+import Layout from "./components/Layout";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route
+          index
+          element={<Home />}
+        />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route path="/*" element={<Error404 />} />
+      </Route>,
+    ),
+  );
+  
   return (
-    <div>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
